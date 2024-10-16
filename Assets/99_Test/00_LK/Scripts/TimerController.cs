17ee7e63dcf38f27831057ef7e9213f92b17d349
kenRhee <71204825+kenRhee@users.noreply.github.com>
2024-10-16 19:24:41 +0900

    feat : 어둠 움직임 구현
    + 대쉬 조작 개선 및 fixedUpdate 추가 스크립트 수정 파일.zip 있음.

---
+++ Assets/99_Test/00_LK/Scripts/TimerController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public float timeLimit = 60f;   // 제한 시간 (초)
+    public float timeRemaining;
+    private bool isTiming = false;
+
+    private TextMesh timerText;     // TextMesh 컴포넌트를 사용할 변수
+    
+    void Start()
+    {
+        timeRemaining = timeLimit;
+        timerText = GetComponent<TextMesh>(); // TextMesh 컴포넌트 가져오기
+        UpdateTimerText();
+        
+        StartTimer();
+    }
+
+    private void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        if (isTiming)
+        {
+            timeRemaining -= Time.deltaTime;
+
+            if (timeRemaining <= 0)
+            {
+                timeRemaining = 0;
+                isTiming = false;
+                GameOver();
+            }
+
+            UpdateTimerText();
+        }
+    }
+
+    public void StartTimer() // 게임 시작시 호출
+    {
+        isTiming = true;
+    }
+
+    public void StopTimer() // 게임 종료시 호출
+    {
+        isTiming = false;
+    }
+
+    private void UpdateTimerText()
+    {
+        timerText.text = "남은 시간: " + Mathf.Ceil(timeRemaining) + "초";
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("게임 오버! 시간 초과!");
+    }
+}
