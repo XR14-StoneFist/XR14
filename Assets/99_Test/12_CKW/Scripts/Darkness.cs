56685b16fe9e1bf6c437f85ca2e78afcf77d563b
Yujun Cho <devhante@gmail.com>
2024-10-18 05:38:22 +0900

    feat: 어둠 체력 감소 기능 추가

--- Assets/99_Test/12_CKW/Scripts/Darkness.cs
+++ Assets/99_Test/12_CKW/Scripts/Darkness.cs
@@ -6,29 +6,52 @@ using Random = UnityEngine.Random;
 
 public class Darkness : MonoBehaviour
 {
+    
     [SerializeField] private GameObject Player;
     [SerializeField] private float ChaseMinValue;
     [SerializeField] private float ChaseMaxValue;
     [SerializeField] private float ChaseSpeed;
     [SerializeField] private float ChaseTime;
+    
+    [Header("Damage")]
+    [SerializeField] private SubCollider DamageCollider;
+
+    [SerializeField] private float DamageCooldown;
+
+    private float _currentDamageCooldown = 0;
 
     private void Start()
     {
         StartCoroutine(ChaseRoutine());
+
+        DamageCollider.OnCollisionStayAction += tagName =>
+        {
+            if (tagName == "Player" && _currentDamageCooldown <= 0)
+            {
+                GameManager.Instance.Health--;
+                _currentDamageCooldown = DamageCooldown;
+            }
+        };
+    }
+
+    private void Update()
+    {
+        _currentDamageCooldown -= Time.deltaTime;
     }
 
     private IEnumerator ChaseRoutine()
     {
-        float chasePosition = Player.transform.position.y + Random.Range(ChaseMinValue, ChaseMaxValue);
-        float chaseTimeLeft = ChaseTime;
-        float chaseSpeed = chasePosition / ChaseTime;
-        while (chaseTimeLeft > 0)
+        while (true)
         {
-            transform.Translate(Vector3.up * (chaseSpeed + ChaseSpeed) * Time.deltaTime);
-            chaseTimeLeft -= Time.deltaTime;
-            yield return null;
+            float chasePosition = Player.transform.position.y + Random.Range(ChaseMinValue, ChaseMaxValue);
+            float chaseTimeLeft = ChaseTime;
+            float chaseSpeed = (chasePosition - transform.position.y) / ChaseTime;
+            while (chaseTimeLeft > 0)
+            {
+                transform.Translate(Vector3.up * (chaseSpeed + ChaseSpeed) * Time.deltaTime);
+                chaseTimeLeft -= Time.deltaTime;
+                yield return null;
+            }
         }
-        
-        StartCoroutine(ChaseRoutine());
     }
 }
